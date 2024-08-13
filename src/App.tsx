8fa0218b33fe996a3ef83934e3c0564961bf14cb
Sergi Tejada <sergitejada0@gmail.com>
2024-08-13 13:20:29 +0200

    refactor: Update App component to use WindowManager for managing windows

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { FaWindows } from "react-icons/fa";
 import "./App.css";
-import Window from "./components/Window";
+import { WindowManager } from "./components/Window";
 
 function App() {
   return (
     <>
       <div className="App">
-        <Window
-          initialWidth={400}
-          initialheight={300}
-          icon={<FaWindows />}
-          title="Mi Ventana"
-        />
+        <WindowManager />
       </div>
     </>
   );
